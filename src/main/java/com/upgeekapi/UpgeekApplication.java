e4b125b32bf16b28332aa1e5f0e6b3aa8829c56b
viniciusdev <viniciusvirada@gmail.com>
2025-06-24 02:07:35 -0300

    inicialização do projeto, estrutura, configuração do banco de dados e primeiro hello world

---
+++ src/main/java/com/upgeekapi/UpgeekApplication.java
@@ -0,0 +1,13 @@
+package com.upgeekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgeekApplication.class, args);
+	}
+
+}
